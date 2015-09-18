fed7af69b2a4f633de5fa83f7fdc9dc70887aec3
Aaron Renoir <aaron@fieldphone.com>
2015-09-17 22:44:05 -0700

    update dummy

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -6,7 +6,7 @@ import {randomNumber, randomDate} from '../utils/random';
 export default Ember.Controller.extend({
   tableColumns: Ember.computed(function() {
     var dateColumn = ColumnDefinition.create({
-      savedWidth: 150,
+      width: 150,
       textAlign: 'text-align-left',
       headerCellName: 'Date',
       getContent: function(row) {
@@ -14,7 +14,7 @@ export default Ember.Controller.extend({
       }
     });
     var openColumn = ColumnDefinition.create({
-      savedWidth: 75,
+      width: 75,
       headerCellName: 'Open',
       isResizable: true,
       getContent: function(row) {
@@ -22,21 +22,21 @@ export default Ember.Controller.extend({
       }
     });
     var highColumn = ColumnDefinition.create({
-      savedWidth: 200,
+      width: 200,
       headerCellName: 'High',
       getContent: function(row) {
         return row.get('high').toFixed(2);
       }
     });
     var lowColumn = ColumnDefinition.create({
-      savedWidth: 200,
+      width: 200,
       headerCellName: 'Low',
       getContent: function(row) {
         return row.get('low').toFixed(2);
       }
     });
     var closeColumn = ColumnDefinition.create({
-      savedWidth: 200,
+      width: 200,
       headerCellName: 'Close',
       getContent: function(row) {
         return row.get('close').toFixed(2);
@@ -48,7 +48,7 @@ export default Ember.Controller.extend({
   tableContent: Ember.computed(function() {
     var content = [];
     var date;
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 5000; i++) {
       date = randomDate(new Date(2000, 1, 5), new Date(2012, 2, 2));
       content.push(Ember.Object.create({
         date: date,
